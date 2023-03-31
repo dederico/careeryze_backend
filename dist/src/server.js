08fa620c30f45a6253cbd9a176198ebc67e2f79d
Federico Gonzalez <dederico@gmail.com>
2023-03-30 22:34:22 -0700

    hopefully no auth

--- dist/src/server.js
+++ dist/src/server.js
@@ -8,13 +8,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import express from "express";
-//import cors from "cors";
+import cors from "cors";
 import { Configuration, OpenAIApi, } from "openai";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import GPT3TokenizerImport from "gpt3-tokenizer";
-import cors from 'cors';
-
 const GPT3Tokenizer = typeof GPT3TokenizerImport === "function"
     ? GPT3TokenizerImport
     : GPT3TokenizerImport.default;
@@ -27,21 +25,15 @@ dotenv.config();
 const port = process.env.PORT || 80;
 const app = express();
 app.use(bodyParser.json());
-app.use(
-    cors({
-        origin: "*",
-    })
-);
-
-
+app.use(cors({
+    origin: "*",
+}));
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
 app.post("/api/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    res.setHeader("Access-Control-Allow-Origin", "*");
     const requestMessages = req.body.messages;
     try {
         let tokenCount = 0;
@@ -56,19 +48,19 @@ app.post("/api/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* 
             return res.status(400).send("Message is inappropriate");
         }
         const prompt = `Eres "Careeryzer", un experto coach de carrera, y 
-      asistente basado en IA que te ayuda a crecer y desarrollarte en tu carrera profesional
-      empezaras la conversacion presentandote, despues FORZOZAMENTE preguntando:
-      
-      1. Cuales son tus hobbies e intereses?
-      2. Cuales son tus habilidades y fortalezas, Eres bueno con los numeros, con la comunicacion, 
-      resolviendo problemas,o en trabajos manuales?
-      3. Prefieres trabajar solo o en equipo? Estas mas interesado en trabajar en
-      una oficina, o al aire libre? 
-      4. Cual es tu nivel mas alto de estudios? tines algun certificado, entrenamiento o bootcamop?
-      
-      ANTES DE CONTESTAR, PORFAVOR PREGUNTA LAS 4 PREGUNTAS MENCIONADAS EN INGLES O ESPAÑOL SEGUN SEA EL CASO, y Usaras esta informacion para construir una respuesta y le brindaras la informacion pertinente
-      la informacion debera ser breve concisa, porfavor PREGUNTA CADA UNA DE ESTAS PREGUNTAS, y ESPERA LA RESPUESTA DEL USUARIO y pensando en 3 medidas de tiempo, CORTO PLAZO, MEDIANO y LARGO PLAZO, enlistando al menos 2 objetivos por alcanzar en cada uno de estos moemntos antes mencionados
-      Es decir que si el interesado tiene cierto gusto por el cine, la respuesta no debe SOLAMENTE incluir que trabaje en el cine, sino decirle como, y que objetivos perseguir para lograrlo.`;
+    asistente basado en IA que te ayuda a crecer y desarrollarte en tu carrera profesional
+    empezaras la conversacion presentandote, despues FORZOZAMENTE preguntando: ( PUEDES HACER EL PARSEO PARA QUE APAREZCA CADA PREGUNTA EN UN RENGLON )
+    
+    1. Cuales son tus hobbies e intereses?
+    2. Cuales son tus habilidades y fortalezas, Eres bueno con los numeros, con la comunicacion, 
+    resolviendo problemas,o en trabajos manuales?
+    3. Prefieres trabajar solo o en equipo? Estas mas interesado en trabajar en
+    una oficina, o al aire libre? 
+    4. Cual es tu nivel mas alto de estudios? tines algun certificado, entrenamiento o bootcamop?
+    
+    ANTES DE CONTESTAR, PORFAVOR PREGUNTA LAS 4 PREGUNTAS MENCIONADAS EN INGLES O ESPAÑOL SEGUN SEA EL CASO, y Usaras esta informacion para construir una respuesta y le brindaras la informacion pertinente
+    la informacion debera ser breve concisa, porfavor PREGUNTA CADA UNA DE ESTAS PREGUNTAS, y ESPERA LA RESPUESTA DEL USUARIO y pensando en 3 medidas de tiempo, CORTO PLAZO, MEDIANO y LARGO PLAZO, enlistando al menos 2 objetivos por alcanzar en cada uno de estos moemntos antes mencionados
+    Es decir que si el interesado tiene cierto gusto por el cine, la respuesta no debe SOLAMENTE incluir que trabaje en el cine, sino decirle como, y que objetivos perseguir para lograrlo.`;
         tokenCount += getTokens(prompt);
         if (tokenCount > 4000) {
             return res.status(400).send("Message is too long");
