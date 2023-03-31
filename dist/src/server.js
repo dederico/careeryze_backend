988377b62fdc70e0ee9bff576cc474fbca1d950c
Federico Gonzalez <dederico@gmail.com>
2023-03-30 22:28:37 -0700

    no auth

--- dist/src/server.js
+++ dist/src/server.js
@@ -8,57 +8,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import { Configuration, OpenAIApi, } from "openai";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import GPT3TokenizerImport from "gpt3-tokenizer";
-// Authentication module import
-import jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt';
-const users = [
-    {
-        email: 'user1@example.com',
-        password: '$2b$10$fSnmSRRJwCwI1gdvKzCUquYI6XjK6mD1iZQ8WZM08rGJbKj70TzvW', // hashed version of 'password'
-    },
-    {
-        email: 'user2@example.com',
-        password: '$2b$10$Pg3qBzYfXjM07nB/iZL7seVRQLcIlyV7QeK/zkmJn7z3iWU9XsUjO', // hashed version of 'password'
-    },
-];
-const jwtSecret = 'your_secret_key';
-const authenticate = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(400).json({ error: 'Please provide an email and password' });
-        return { token: '' };
-    }
-    const user = users.find((u) => u.email === email);
-    if (!user) {
-        res.status(401).json({ error: 'Invalid email or password' });
-        return { token: '' };
-    }
-    const match = yield bcrypt.compare(password, user.password);
-    if (!match) {
-        res.status(401).json({ error: 'Invalid email or password' });
-        return { token: '' };
-    }
-    const token = jwt.sign({ email }, jwtSecret);
-    res.json({ token });
-    return { token };
-});
-const verifyToken = (token) => {
-    return new Promise((resolve, reject) => {
-        jwt.verify(token, jwtSecret, (err, decoded) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(decoded);
-            }
-        });
-    });
-};
+import cors from 'cors';
+
 const GPT3Tokenizer = typeof GPT3TokenizerImport === "function"
     ? GPT3TokenizerImport
     : GPT3TokenizerImport.default;
@@ -68,37 +24,24 @@ function getTokens(input) {
     return tokens.text.length;
 }
 dotenv.config();
-const port = 8000;
+const port = process.env.PORT || 80;
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({
-    origin: "*",
-}));
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-app.post("/api/authenticate", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password } = req.body;
-    try {
-        // Check if user exists and password is correct
-        const user = yield authenticate(email, password);
-        if (user) {
-            // Generate token with JWT
-            const token = jwt.sign({ id: user.token }, process.env.JWT_SECRET || "");
-            // Send token to client
-            res.json({ token });
-        }
-        else {
-            res.status(401).send("Invalid credentials");
-        }
-    }
-    catch (error) {
-        res.status(500).send("Something went wrong");
-    }
-}));
+
 app.post("/api/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
+    res.setHeader("Access-Control-Allow-Origin", "*");
     const requestMessages = req.body.messages;
     try {
         let tokenCount = 0;
@@ -141,7 +84,7 @@ app.post("/api/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* 
     catch (error) {
         if (error instanceof Error) {
             // @ts-ignore
-            console.log(error.toJSON());
+            console.error(error);
         }
         res.status(500).send("Something went wrong");
     }
