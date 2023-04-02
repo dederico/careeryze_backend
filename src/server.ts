ff9c638fffb492b4de74e6ad729a667d172f1551
fedeTeam <federico.davila@teamsnap.com>
2023-04-01 21:54:07 -0700

    new prompting

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ import {
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import GPT3TokenizerImport from "gpt3-tokenizer";
-import { AxiosRequestConfig } from "axios";
 
 const GPT3Tokenizer: typeof GPT3TokenizerImport =
   typeof GPT3TokenizerImport === "function"
@@ -19,7 +18,6 @@ const GPT3Tokenizer: typeof GPT3TokenizerImport =
 const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
 
 function getTokens(input: string): number {
-  input = input.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   const tokens = tokenizer.encode(input);
   return tokens.text.length;
 }
@@ -40,108 +38,80 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-// Define an array of questions
-const questions = [
-  "What are your hobbies and interests?",
-  "What are your skills and strengths? Are you good with numbers, communication, problem solving, or hands-on work?",
-  "Do you prefer to work alone or in a team? Are you more interested in working in an office or outdoors?",
-  "What is your highest level of education? Do you have any certificates, trainings, or bootcamps?",
-];
-
-// Define a function to get the next question based on the current index
-function getNextQuestion(currentIndex: number): string {
-  if (currentIndex < questions.length) {
-    const question = questions[currentIndex].replace(/[\r\n]+/g, "");
-    return question;
-  }
-  return "We're done with the questions. Is there anything else I can help you with?";
-}
-
-// Define a variable to keep track of the current question index
-let currentQuestionIndex = 0;
-
-// Define the app.post endpoint
 app.post("/api/chat", async (req: Request, res: Response) => {
   const requestMessages: ChatCompletionRequestMessage[] = req.body.messages;
-{
-}
+
   try {
     let tokenCount = 0;
+    let originalPrompt = `Eres "Careeryzer", 
+    un experto coach de carrera, y asistente basado en IA que 
+    te ayuda a crecer y desarrollarte en tu carrera profesional. `;
+
+    // NamePrompt
+    const namePrompt = `Por favor, presentate, y dime tu nombre.`;
+    tokenCount+= getTokens(namePrompt);
+    originalPrompt +=namePrompt;
+    
+    // Hobbies Prompt
+    const hobbiesPrompt = `Cuales son tus hobbies e intereses?`;
+    tokenCount+= getTokens(hobbiesPrompt);
+    originalPrompt += namePrompt + hobbiesPrompt ;
+
+    // Skills Prompt
+    const skillPrompt = `Cuales son las habilidades y fortalezas?`;
+    tokenCount+= getTokens(skillPrompt);
+    originalPrompt += namePrompt + hobbiesPrompt + skillPrompt;
+
+    // Work Experience
+    const experiencePrompt = `Cual es tu experiencia en la industria?`
+    tokenCount+= getTokens(experiencePrompt);
+    originalPrompt += namePrompt + hobbiesPrompt + skillPrompt + experiencePrompt;
 
     requestMessages.forEach((msg) => {
       const tokens = getTokens(msg.content);
       tokenCount += tokens;
+      originalPrompt += `${msg.content}`
     });
-
-    const authHeader: { Authorization?: string } = process.env.AUTH ? { Authorization: process.env.AUTH } : {};
-    const options: AxiosRequestConfig =  {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: 'application/json, text/plain, */*',
-        'User-Agent': 'OpenAI/NodeJS/3.2.1',
-        'Content-Length': 264,
-        ...authHeader
-      },
-    };
 
     const moderationResponse = await openai.createModeration({
       input: requestMessages[requestMessages.length - 1].content,
-    }, options);
+    });
     if (moderationResponse.data.results[0]?.flagged) {
       return res.status(400).send("Message is inappropriate");
     }
 
-    // If we haven't finished all the questions, get the next question and update the current question index
-    if (currentQuestionIndex < questions.length) {
-      const prompt = getNextQuestion(currentQuestionIndex);
-      tokenCount += getTokens(prompt);
-      if (tokenCount > 4000) {
-        return res.status(400).send("Message is too long");
-      }
+    const prompt =
+      `Eres "Careeryzer", un experto coach de carrera, y 
+    asistente basado en IA que te ayuda a crecer y desarrollarte en tu carrera profesional
+    empezaras la conversacion presentandote, despues FORZOZAMENTE preguntando: ( PUEDES HACER EL PARSEO PARA QUE APAREZCA CADA PREGUNTA EN UN RENGLON )
+    
+    1. Cuales son tus hobbies e intereses?
+    2. Cuales son tus habilidades y fortalezas, Eres bueno con los numeros, con la comunicacion, 
+    resolviendo problemas,o en trabajos manuales?
+    3. Prefieres trabajar solo o en equipo? Estas mas interesado en trabajar en
+    una oficina, o al aire libre? 
+    4. Cual es tu nivel mas alto de estudios? tines algun certificado, entrenamiento o bootcamop?
+    
+    ANTES DE CONTESTAR, PORFAVOR PREGUNTA LAS 4 PREGUNTAS MENCIONADAS EN INGLES O ESPAÑOL SEGUN SEA EL CASO, y Usaras esta informacion para construir una respuesta y le brindaras la informacion pertinente
+    la informacion debera ser breve concisa, porfavor PREGUNTA CADA UNA DE ESTAS PREGUNTAS, y ESPERA LA RESPUESTA DEL USUARIO y pensando en 3 medidas de tiempo, CORTO PLAZO, MEDIANO y LARGO PLAZO, enlistando al menos 2 objetivos por alcanzar en cada uno de estos moemntos antes mencionados
+    Es decir que si el interesado tiene cierto gusto por el cine, la respuesta no debe SOLAMENTE incluir que trabaje en el cine, sino decirle como, y que objetivos perseguir para lograrlo.`;
 
-      const apiRequestBody: CreateChatCompletionRequest = {
-        model: "text-davinci-002",
-        messages: [
-          { role: "system", content: " Gracias por responser todas las preguntas"},
-          ...requestMessages.map(({ role, content }) => ({ role, content: content.replace(/[\r\n]+/g, "") }))
-        ],
-        max_tokens: 150,
-        temperature: 0.5,
-        n: 1,
-        stream: false,
-        stop: ["\n"],
-        presence_penalty: 0.6,
-        frequency_penalty: 0.6,
-      };
-      const completion = await openai.createCompletion(apiRequestBody,options);
-
-      currentQuestionIndex++;
-      res.json({ message: completion.data.choices[0].text?.trim() });
+    tokenCount += getTokens(prompt+originalPrompt);
+    if (tokenCount > 4000) {
+      return res.status(400).send("Message is too long");
     }
 
-    // Otherwise, if we have finished all the questions, just send a confirmation message
-
     const apiRequestBody: CreateChatCompletionRequest = {
-      model: "text-davinci-002",
-      messages: [
-
-        {role: "system", content: "Gracias por responder todas las preguntas."},
-        ...requestMessages.map(({ role, content }) => ({ role, content: content.replace(/[\r\n]+/g, "") }))
-      ],
-      max_tokens: 100,
-      temperature: 0.5,
-      n: 1,
-      stream: false,
-      stop: ["\n"],
-      presence_penalty: 0.6,
-      frequency_penalty: 0.6,
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "system", content: prompt+originalPrompt }, ...requestMessages],
+      temperature: 0.6,
     };
-    const completion = await openai.createCompletion(apiRequestBody, options);
+    const completion = await openai.createChatCompletion(apiRequestBody);
 
-    res.json({ message: completion.data.choices[0].text?.trim() });
-
+    res.json(completion.data);
   } catch (error) {
     if (error instanceof Error) {
+      // @ts-ignore
       console.error(error);
     }
     res.status(500).send("Something went wrong");
